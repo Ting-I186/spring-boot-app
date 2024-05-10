0973c84bb7229af5d894c939d3df786c1dfedd99
Ting-I Liang <ting-i.liang@entelect.co.za>
2024-05-10 13:23:19 +0200

    Added logging to the app

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
     @GetMapping("/")
     public String index() {
+        log.info("Hello world endpoint requested.");
         return "Hello world!";
     }
 }
