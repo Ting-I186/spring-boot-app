3aca1834fc8abbc8f0424043eccd8bc1dd4e1e0f
Ting-I Liang <ting-i.liang@entelect.co.za>
2024-05-10 11:35:03 +0200

    Added application configurations and a controller to get those values

---
+++ src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,31 @@
+package com.example.demo.models;
+
+public class Person {
+    private String name;
+    private String surname;
+
+//    public Person(String name, String surname){
+//        this.name = name;
+//        this.surname = surname;
+//    }
+//
+//    public Person() {
+//
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
