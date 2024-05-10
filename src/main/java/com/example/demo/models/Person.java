6c8d95681466f0d780a80785279b13ba8f256a87
Ting-I Liang <ting-i.liang@entelect.co.za>
2024-05-10 11:59:39 +0200

    Removed unused code

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -4,15 +4,6 @@ public class Person {
     private String name;
     private String surname;
 
-//    public Person(String name, String surname){
-//        this.name = name;
-//        this.surname = surname;
-//    }
-//
-//    public Person() {
-//
-//    }
-
     public String getName() {
         return name;
     }
